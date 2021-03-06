d2fa5b532c7d432bdb967e0cf16c9e10477e96f1
sametakgoz <79753522+sametakgoz@users.noreply.github.com>
2021-03-06 16:54:17 +0300

    Add project files.

---
+++ day4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> products = new MyList<string>();
+            products.Add("Kalem");
+            products.Add("silgi");
+            Console.WriteLine();
+            
+
+        }
+    }
+}
